5eaf245102695e6d53a546fe663ff448f4c7d2be
RyanVoccio <ryanpvoccio@gmail.com>
2023-05-31 00:11:20 -0500

    editing test

--- src/tests/PrimeNumberTest.java
+++ src/tests/PrimeNumberTest.java
@@ -13,11 +13,8 @@ public class PrimeNumberTest {
 
 	@Test
 	public void test() {
-		//fail("Not yet implemented");
 
         System.out.println("This is the testcase in this class");
-        String str1="This is the testcase in this class";
-        assertEquals("This is the testcase in this class", str1);
         
         PrimeCalculator calculator = new PrimeCalculator();
 		
